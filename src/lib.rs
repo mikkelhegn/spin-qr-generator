b8b90cf1f42fc228cafac63b7573a70910f7d5a3
Mikkel Mørk Hegnhøj <mikkel@fermyon.com>
2023-04-04 10:55:42 +0200

    Init
    Signed-off-by: Mikkel Mørk Hegnhøj <mikkel@fermyon.com>

---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+use anyhow::Result;
+use bytes::Bytes;
+use http::StatusCode;
+use percent_encoding::percent_decode;
+use qrcode_generator::QrCodeEcc;
+use spin_sdk::{
+    http::{Request, Response},
+    http_component,
+};
+
+/// A simple Spin HTTP component.
+#[http_component]
+fn handle_qr_discord(req: Request) -> Result<Response> {
+    let (status, body) = match req.uri().query() {
+        Some(p) => {
+            let decoded_query = percent_decode(p.as_bytes()).decode_utf8();
+            match qrcode_generator::to_svg_to_string(
+                decoded_query.unwrap().as_ref(),
+                QrCodeEcc::Medium,
+                512,
+                None::<String>,
+            ) {
+                Ok(q) => (StatusCode::OK, Some(Bytes::from(q))),
+                _ => (StatusCode::INTERNAL_SERVER_ERROR, None),
+            }
+        }
+        _ => (StatusCode::NOT_FOUND, None),
+    };
+
+    Ok(http::Response::builder().status(status).body(body)?)
+}
